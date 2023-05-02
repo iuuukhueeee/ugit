749e408eea93eafa98b39dad2bac68afc42df84a
iuuukhueeee <nguyenducthien9@gmail.com>
2023-05-02 15:20:16 +0700

    cli: added argument parser

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,2 +1,22 @@
-def main():
+import argparse
+
+
+def init(args):
     print('Goodbye World')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    commands = parser.add_subparsers(dest='command')
+    commands.required = True
+
+    init_parser = commands.add_parser('init')
+    init_parser.set_defaults(default_function=init)
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    print(args, end='\n')
+    args.default_function(args)
