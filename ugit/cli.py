2bd47629ef9803865ffe689284979ab90252bed8
iuuukhueeee <nguyenducthien9@gmail.com>
2023-05-03 15:17:43 +0700

    cat-file: print hashed object

--- ugit/cli.py
+++ ugit/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import sys
 from . import data
 
 
@@ -15,6 +16,10 @@ def parse_args():
     hash_object_parser.set_defaults(default_function=hash_object)
     hash_object_parser.add_argument('file')
 
+    cat_file_parser = commands.add_parser('cat-file')
+    cat_file_parser.set_defaults(default_function=cat_file)
+    cat_file_parser.add_argument('object')
+
     return parser.parse_args()
 
 
@@ -28,8 +33,11 @@ def hash_object(args):
     with open(args.file, 'rb') as f:
         print(data.hash_object(f.read()))
 
+def cat_file(args):
+    sys.stdout.flush()
+    sys.stdout.buffer.write(data.get_object(args.object))
+
 
 def main():
     args = parse_args()
-    print(args, end='\n')
     args.default_function(args)
